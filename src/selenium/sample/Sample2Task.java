b4ba2229d41bfc7fb8f57a7ffc63b37046c06750
uljanovs <a.uljanovs@gmail.com>
2020-03-01 16:31:55 +0200

    Update Sample2Task.java

--- src/selenium/sample/Sample2Task.java
+++ src/selenium/sample/Sample2Task.java
@@ -23,7 +23,7 @@ public class Sample2Task {
         driver = new ChromeDriver();
 
         //open page:
-        driver.get("https://kristinek.github.io/site/examples/locators");
+        driver.get("https://uljanovs.github.io/site/examples/locators");
     }
 
     // method which is being run after each test
