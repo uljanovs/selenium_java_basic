2c1c92de22c3b594178c5c0bb89d6b4450e69ff4
Artjoms <corsatomy@gmail.com>
2020-03-02 11:52:01 +0200

    SampleTask - completed 2

--- src/selenium/sample/Sample1Task.java
+++ src/selenium/sample/Sample1Task.java
@@ -13,7 +13,7 @@ public class Sample1Task {
         System.setProperty("webdriver.chrome.driver", libWithDriversLocation + "chromedriver.exe");
         WebDriver browser = new ChromeDriver();
 
-//       TODO:  go to https://uljanovs.github.io/site/index2.htmlddd
+//       TODO:  go to https://uljanovs.github.io/site/index2.htmldddd
         browser.get("https://uljanovs.github.io/site/index2.html");
 
 //         get title of page
