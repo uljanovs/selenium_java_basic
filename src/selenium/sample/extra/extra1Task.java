3e206244515fae973b446fc21ed98ff3258d922b
vjuzhka <olga@gmail.com>
2020-03-03 11:51:17 +0200

    extra1Task - completed

--- src/selenium/sample/extra/extra1Task.java
+++ src/selenium/sample/extra/extra1Task.java
@@ -3,8 +3,11 @@ package selenium.sample.extra;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.*;
 
 public class extra1Task {
     WebDriver driver;
@@ -38,6 +41,14 @@ public class extra1Task {
 //        check that the url now "https://uljanovs.github.io/site/examples/po1"
 //        using driver navigation go back to "https://uljanovs.github.io/site/examples/po"
 //        check that the page now is "https://uljanovs.github.io/site/examples/po"
+
+        driver.get("https://uljanovs.github.io/site/examples/po");
+        driver.findElement(By.linkText("po1")).click();
+        assertEquals("https://uljanovs.github.io/site/examples/po1", driver.getCurrentUrl());
+        driver.navigate().back();
+        assertEquals("https://uljanovs.github.io/site/examples/po", driver.getCurrentUrl());
+
+
     }
 
     @Test
@@ -48,6 +59,14 @@ public class extra1Task {
 //        using driver navigation go back to "https://uljanovs.github.io/site/examples/po"
 //        using driver navigation go forward to "https://uljanovs.github.io/site/examples/po1"
 //        check that the page now is "https://uljanovs.github.io/site/examples/po1"
+
+        driver.get("https://uljanovs.github.io/site/examples/po");
+        driver.findElement(By.linkText("po1")).click();
+        driver.navigate().back();
+        driver.navigate().forward();
+        assertEquals("https://uljanovs.github.io/site/examples/po1", driver.getCurrentUrl());
+
+
     }
 
     @Test
@@ -58,5 +77,12 @@ public class extra1Task {
 //        check that text "I am here!" is seen
 //        refresh page
 //        check that text "I am here!" is not seen
+        driver.get("https://uljanovs.github.io/site/examples/act");
+        driver.findElement(By.id("show_text")).click();
+        assertEquals("I am here!", driver.findElement(By.id("show_me")).getText());
+        driver.navigate().refresh();
+        assertFalse(driver.findElement(By.id("show_me")).isDisplayed());
+
+
     }
 }
