e9e759433255312c572c099a061a6a24850173e4
vjuzhka <olga@gmail.com>
2020-03-03 10:18:16 +0200

    Sample8Task - completed

--- src/selenium/sample/Sample8Task.java
+++ src/selenium/sample/Sample8Task.java
@@ -39,8 +39,18 @@ public class Sample8Task {
                 driver.findElement(By.cssSelector(".w3-pale-red")).getCssValue("background-color"));
         assertEquals("rgba(255, 255, 204, 1)",
                 driver.findElement(By.cssSelector(".w3-pale-yellow")).getCssValue("background-color"));
+        assertEquals("rgba(221, 255, 255, 1)",
+                driver.findElement(By.cssSelector(".w3-pale-blue")).getCssValue("background-color"));
 //        check h1 element font-size 64px
         assertEquals("64px",
                 driver.findElement(By.cssSelector("h1")).getCssValue("font-size"));
+        assertEquals("15px",
+                driver.findElement(By.cssSelector("div")).getCssValue("font-size"));
+        assertEquals("Verdana, sans-serif",
+                driver.findElement(By.cssSelector("div")).getCssValue("font-family"));
+        assertEquals("22.5px",
+                driver.findElement(By.cssSelector("div")).getCssValue("line-height"));
+
+
     }
 }
