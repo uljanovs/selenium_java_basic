e3e630978465782bf565e71bc11eeb80421aacac
liten-kersti <tamiteatime@gmail.com>
2020-03-03 11:04:12 +0200

    extra1Task - completed

--- src/selenium/sample/extra/extra1Task.java
+++ src/selenium/sample/extra/extra1Task.java
@@ -3,8 +3,12 @@ package selenium.sample.extra;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class extra1Task {
     WebDriver driver;
@@ -34,29 +38,44 @@ public class extra1Task {
     public void navigateBack() throws Exception {
 //        TODO
 //        open page with url "https://uljanovs.github.io/site/examples/po"
+        driver.get("https://uljanovs.github.io/site/examples/po");
 //        click "More > " for the top left element
+        driver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/p[2]/a")).click();
 //        check that the url now "https://uljanovs.github.io/site/examples/po1"
+        assertEquals("https://uljanovs.github.io/site/examples/po1", driver.getCurrentUrl());
 //        using driver navigation go back to "https://uljanovs.github.io/site/examples/po"
+        driver.navigate().back();
 //        check that the page now is "https://uljanovs.github.io/site/examples/po"
+        assertEquals("https://uljanovs.github.io/site/examples/po", driver.getCurrentUrl());
     }
 
     @Test
     public void navigateForward() throws Exception {
 //        TODO
 //        open page with url "https://uljanovs.github.io/site/examples/po"
+        driver.get("https://uljanovs.github.io/site/examples/po");
 //        click "More > " for the top left element
+        driver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div/p[2]/a")).click();
 //        using driver navigation go back to "https://uljanovs.github.io/site/examples/po"
+        driver.navigate().back();
 //        using driver navigation go forward to "https://uljanovs.github.io/site/examples/po1"
+        driver.navigate().forward();
 //        check that the page now is "https://uljanovs.github.io/site/examples/po1"
+        assertEquals("https://uljanovs.github.io/site/examples/po1", driver.getCurrentUrl());
     }
 
     @Test
     public void refresh() throws Exception {
 //        TODO
 //        open page "https://uljanovs.github.io/site/examples/act"
+        driver.get("https://uljanovs.github.io/site/examples/act");
 //        click on "Show" button in 'Button' section
+        driver.findElement(By.id("show_text")).click();
 //        check that text "I am here!" is seen
+        assertEquals("I am here!", driver.findElement(By.id("show_me")).getText());
 //        refresh page
+        driver.navigate().refresh();
 //        check that text "I am here!" is not seen
+        assertFalse( driver.findElement(By.id("show_me")).isDisplayed());
     }
 }
