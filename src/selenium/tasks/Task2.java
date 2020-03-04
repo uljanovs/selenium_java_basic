e28dfef42495f0ca7ef28233162d5d316895c483
ingarskrafts <ingarskrafts@gmail.com>
2020-03-04 22:23:34 +0200

    Task2 completed

--- src/selenium/tasks/Task2.java
+++ src/selenium/tasks/Task2.java
@@ -47,6 +47,9 @@ public class Task2 {
 //         "Choose your option" in "How do you like us?"
         assertTrue("Choose your option", driver.findElement(By.cssSelector("#like_us > option:nth-child(1)")).isSelected());
 //         check that the button send is blue with white letters
+        assertEquals("rgba(33, 150, 243, 1)", driver.findElement(By.cssSelector("#fb_form > form > button")).getCssValue("background-color"));
+        assertEquals("rgba(255, 255, 255, 1)", driver.findElement(By.cssSelector("#fb_form > form > button")).getCssValue("color"));
+
     }
 
     @Test
@@ -57,7 +60,7 @@ public class Task2 {
         WebElement start = driver.findElement(By.cssSelector("#fb_form > form > button"));
         start.click();
 //         check fields are empty or null
-           assertEquals("", driver.findElement(By.cssSelector("#name")).getText());
+        assertEquals("", driver.findElement(By.cssSelector("#name")).getText());
         assertEquals("", driver.findElement(By.cssSelector("#age")).getText());
         assertEquals("", driver.findElement(By.cssSelector("#language")).getText());
         assertEquals("null", driver.findElement(By.cssSelector("#gender")).getText());
@@ -72,15 +75,21 @@ public class Task2 {
     public void notEmptyFeedbackPage() throws Exception {
 //         TODO:
 //         fill the whole form, click "Send"
-        driver.findElement(By.cssSelector("#name")).sendKeys("Ingars");
-        driver.findElement(By.cssSelector("#age")).sendKeys("35");
+        driver.findElement(By.cssSelector("#fb_name")).sendKeys("Ingars");
+        driver.findElement(By.cssSelector("#fb_age")).sendKeys("35");
         driver.findElement(By.cssSelector("#lang_check > input:nth-child(2)")).click();
-       Select likeUs = new Select (driver.findElement(By.cssSelector("#like_us")));
+        driver.findElement(By.cssSelector("#fb_form > form > div:nth-child(4) > input:nth-child(2)")).click();
+        Select likeUs = new Select(driver.findElement(By.cssSelector("#like_us")));
         likeUs.selectByIndex(1);
+        driver.findElement(By.cssSelector("#fb_form > form > button")).click();
 
 //         check fields are filled correctly
-
-//         check button colors
+        assertEquals("Ingars", driver.findElement(By.cssSelector("#name")).getText());
+        assertEquals("35", driver.findElement(By.cssSelector("#age")).getText());
+        assertEquals("English", driver.findElement(By.cssSelector("#language")).getText());
+        assertEquals("male", driver.findElement(By.cssSelector("#gender")).getText());
+        assertEquals("Good", driver.findElement(By.cssSelector("#option")).getText());
+        //         check button colors
 //         (green with white letter and red with white letters)
     }
 
@@ -88,9 +97,13 @@ public class Task2 {
     public void yesOnWithNameFeedbackPage() throws Exception {
 //         TODO:
 //         enter only name
+        driver.findElement(By.cssSelector("#fb_name")).sendKeys("Ingars");
 //         click "Send"
+        driver.findElement(By.cssSelector("#fb_form > form > button")).click();
 //         click "Yes"
+        driver.findElement(By.cssSelector("#fb_thx > div > div.w3-btn-group > button.w3-btn.w3-green.w3-xlarge")).click();
 //         check message text: "Thank you, NAME, for your feedback!"
+        assertEquals("Thank you, Ingars, for your feedback!", driver.findElement(By.xpath("//*[@id=\"message\"]")).getText());
 //         color of text is white with green on the background
     }
 
@@ -98,8 +111,11 @@ public class Task2 {
     public void yesOnWithoutNameFeedbackPage() throws Exception {
 //         TODO:
 //         click "Send" (without entering anything
+        driver.findElement(By.cssSelector("#fb_form > form > button")).click();
 //         click "Yes"
+        driver.findElement(By.cssSelector("#fb_thx > div > div.w3-btn-group > button.w3-btn.w3-green.w3-xlarge")).click();
 //         check message text: "Thank you for your feedback!"
+        assertEquals("Thank you for your feedback!", driver.findElement(By.xpath("//*[@id=\"message\"]")).getText());
 //         color of text is white with green on the background
     }
 
@@ -107,8 +123,22 @@ public class Task2 {
     public void noOnFeedbackPage() throws Exception {
 //         TODO:
 //         fill the whole form
+        driver.findElement(By.cssSelector("#fb_name")).sendKeys("Ingars");
+        driver.findElement(By.cssSelector("#fb_age")).sendKeys("35");
+        driver.findElement(By.cssSelector("#lang_check > input:nth-child(2)")).click();
+        driver.findElement(By.cssSelector("#fb_form > form > div:nth-child(4) > input:nth-child(2)")).click();
+        Select likeUs = new Select(driver.findElement(By.cssSelector("#like_us")));
+        likeUs.selectByIndex(1);
 //         click "Send"
+        driver.findElement(By.cssSelector("#fb_form > form > button")).click();
 //         click "No"
+        driver.findElement(By.cssSelector("#fb_thx > div > div.w3-btn-group > button.w3-btn.w3-red.w3-xlarge")).click();
 //         check fields are filled correctly
+        assertEquals("Ingars", driver.findElement(By.cssSelector("#fb_name")).getAttribute("value"));
+        assertEquals("35", driver.findElement(By.cssSelector("#fb_age")).getAttribute("value"));
+        assertTrue(driver.findElement(By.cssSelector("#lang_check > input:nth-child(2)")).isSelected());
+        assertTrue(driver.findElement(By.cssSelector("#fb_form > form > div:nth-child(4) > input:nth-child(2)")).isSelected());
+        assertEquals("Good", driver.findElement(By.cssSelector("#like_us")).getAttribute("value"));
+
     }
 }
